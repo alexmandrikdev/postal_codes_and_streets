48337d3e9ae4da95655b68b202c5fbe1bbe950ce
MnDAlex <alexvok12@gmail.com>
2021-02-05 22:34:17 +0100

    Add index method for streets route

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('postal_codes', PostalCodeController::class)->only(['store']);
 
-    Route::apiResource('streets', StreetController::class)->only(['store']);
+    Route::apiResource('streets', StreetController::class)->only(['index', 'store']);
 });
