8b3a6028c80c6565a99c385f78c9a93d7ff5112e
MnDAlex <alexvok12@gmail.com>
2021-02-05 19:58:16 +0100

    Create CreatePostalCodesTable migration

---
+++ database/migrations/2021_02_05_152746_create_postal_codes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostalCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postal_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('postal_code', 5);
+            $table->string('locality');
+            $table->decimal('latitude', 9, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postal_codes');
+    }
+}
