964a3f124a639f2231603fdbfbf8acb062fe58e3
MnDAlex <alexvok12@gmail.com>
2021-02-05 12:36:21 +0100

    Install BootstrapVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,16 @@
-require("./bootstrap");
+require('./bootstrap');
 
-import Vue from "vue";
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import App from "./components/App.vue";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import App from './components/App.vue';
 
 const app = new Vue({
-    el: "#app",
+    el: '#app',
     components: { App },
 });
