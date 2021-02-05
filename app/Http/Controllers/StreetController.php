a7e2200362ab3752abe13455a1ed5f09f57d407b
MnDAlex <alexvok12@gmail.com>
2021-02-05 22:33:43 +0100

    Create index function for StreetController

--- app/Http/Controllers/StreetController.php
+++ app/Http/Controllers/StreetController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StreetIndexRequest;
 use App\Http\Requests\StreetStoreRequest;
 use App\Models\Street;
 
 class StreetController extends Controller
 {
+    public function index(StreetIndexRequest $request)
+    {
+        $query = Street::query();
+
+        if ($request->postal_code !== null && $request->postal_code !== '') {
+            $query->where('postal_code', 'LIKE', $request->postal_code . "%");
+        }
+
+        if ($request->name !== null && $request->name !== '') {
+            $query->where('name', 'LIKE', $request->name . "%");
+        }
+
+        return $query->with('postalCode2')->get();
+    }
+
     public function store(StreetStoreRequest $request)
     {
         return Street::create($request->validated());
