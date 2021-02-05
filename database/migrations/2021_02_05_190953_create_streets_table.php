1801b8a861a8e6ab9139613e95b1bd50e64853de
MnDAlex <alexvok12@gmail.com>
2021-02-05 20:32:25 +0100

    Create CreateStreetsTable migration

---
+++ database/migrations/2021_02_05_190953_create_streets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStreetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('streets', function (Blueprint $table) {
+            $table->id();
+            $table->string('postal_code', 5);
+            $table->string('name');
+            $table->decimal('latitude', 9, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('streets');
+    }
+}
