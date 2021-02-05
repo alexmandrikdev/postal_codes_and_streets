10f50aa264d48024a8dc838c60d19512b126b6f0
MnDAlex <alexvok12@gmail.com>
2021-02-05 20:34:16 +0100

    Add streets route to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\PostalCodeController;
+use App\Http\Controllers\StreetController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('postal_codes', PostalCodeController::class)->only(['store']);
+
+    Route::apiResource('streets', StreetController::class)->only(['store']);
 });
