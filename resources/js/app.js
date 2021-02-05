3e1d3fc49e16b128bf74c9d862def9e877a29ce4
MnDAlex <alexvok12@gmail.com>
2021-02-05 12:40:45 +0100

    Set up VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import router from './router';
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -8,9 +10,9 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-import App from './components/App.vue';
+import App from './App.vue';
 
-const app = new Vue({
-    el: '#app',
-    components: { App },
-});
+new Vue({
+    router,
+    render: (h) => h(App),
+}).$mount('#app');
